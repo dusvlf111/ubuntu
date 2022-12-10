596d85e9e8e0c8fbbb0346a5e759ce6fcdc03190
dusvlf111 <dusvlf5950@naver.com>
2022-12-11 03:48:59 +0900

    12110348

--- .sungbin/GUI/8_combo.py
+++ .sungbin/GUI/8_combo.py
@@ -8,15 +8,24 @@ root.title("sungbin GUI")
 root.geometry("640x480")
 
 values = [str(i) + "day" for i in range(1, 32)]
-Combobox = Combobox(root, height=5, values=values)
-Combobox.pack()
-Combobox.set("select card pay day")
+combobox = Combobox(root, height=5, values=values)
+combobox.pack()
+combobox.set("select card pay day")
+
+values = [str(i) + "day" for i in range(1, 32)]
+read_Combobox = Combobox(root, height=10, values=values, state = "readonly")
+read_Combobox.current(0)
+read_Combobox.pack()
+
+
 
 
 
 
 def btncmd():
-    pass
+    print(combobox.get())
+    print(read_Combobox.get())
+
 
 btn = Button(root, text="click", command=btncmd)
 btn.pack()
